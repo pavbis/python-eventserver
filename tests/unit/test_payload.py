5272b83d12b2b4419fe5c8efd846c65af502ae39
Pavel Bisse <pavel@billomat.com>
2020-03-06 10:00:32 +0100

    remove payload to_json call

--- tests/unit/test_payload.py
+++ tests/unit/test_payload.py
@@ -1,13 +1,10 @@
 from unittest import TestCase
-from eventsserver.value.objects import PayLoad, InvalidArgument
+from eventsserver.value.objects import PayLoad
 
 
 class PayloadTest(TestCase):
     def setUp(self) -> None:
         self.payload = PayLoad({'one': 1, 'two': 2, 'three': {'three.1': 3.1, 'three.2': 3.2}})
-
-    def test_it_returns_provided_primitive_value_as_json(self):
-        self.assertEqual('{"one": 1, "two": 2, "three": {"three.1": 3.1, "three.2": 3.2}}', self.payload.to_json())
 
     def test_it_returns_provided_primitive_value(self):
         self.assertEqual("{'one': 1, 'two': 2, 'three': {'three.1': 3.1, 'three.2': 3.2}}", str(self.payload))
